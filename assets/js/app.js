617ebcbd5fd86f9efe9f419c4fe58286fc5d3d15
musicteachj <jimilittle3@hotmail.com>
2018-05-18 19:35:01 -0400

    add styles, structure and functionality
    1st draft

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-console.log('Working');
+var num = [];
+var reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+document.getElementById('numberC').innerHTML = num;
+
+document.getElementById('btn').addEventListener("click", function() {
+	var newValue = document.getElementById('input').value;
+
+	document.getElementById('input').value = "";
+
+	var numT = parseInt(newValue);
+	num.push(numT)
+
+	document.getElementById('numberC').innerHTML = num;
+
+	var numAdd = num.reduce(reducer);
+
+	var average = numAdd / num.length;
+
+	console.log(average);
+
+	document.getElementById('averageC').innerHTML = average.toFixed(1);
+	
+});
+
+
+
+	
