74001c098da333ed822695b0da3bd1c34fce683f
musicteachj <jimilittle3@hotmail.com>
2018-05-18 20:08:24 -0400

    add reset func and conditional
    add reset functionality for averages and added if statement for if user enters NaN

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 var num = [];
 var reducer = (accumulator, currentValue) => accumulator + currentValue;
+var newValue = "";
 
 document.getElementById('numberC').innerHTML = num;
 
 document.getElementById('btn').addEventListener("click", function() {
 	var newValue = document.getElementById('input').value;
 
-	document.getElementById('input').value = "";
+	if (newValue === "") {
+		alert("Please enter a number");
+	} else {
+			document.getElementById('input').value = "";
 
 	var numT = parseInt(newValue);
 	num.push(numT)
@@ -17,12 +21,22 @@ document.getElementById('btn').addEventListener("click", function() {
 
 	var average = numAdd / num.length;
 
-	console.log(average);
-
 	document.getElementById('averageC').innerHTML = average.toFixed(1);
+	}
+
+
 	
 });
 
+document.getElementById('btnTwo').addEventListener("click", function() {
+	reset();
+});
 
+function reset() {
+		num = [];
+	document.getElementById('input').value = "";
+	document.getElementById('numberC').innerHTML = "";
+	document.getElementById('averageC').innerHTML = "";
+}
 
 	
